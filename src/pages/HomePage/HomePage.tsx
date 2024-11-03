7e242bde0b451ed772e3425758d9dee283bb5384
Matsvei Khmialnitski <geniusx1990@gmail.com>
2024-11-03 14:39:52 +0300

    feat: added home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,28 @@
 import { getImgUrl } from "../../utils/getImageUrl";
+import './style.css';
 
 export default function HomePage() {
     return (
 
-        <section>
-            <div>
-                <h1>Beautiful food & takeaway, delivered to your door.</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
-            </div>
-            <button>Place an Order</button>
-            <div>
-                <img src={getImgUrl('star.svg')} alt="" />
-                <p>Trustpilot</p>
-                <div><p>4.8 out of 5</p>
-                    <p>based on 2000+ reviews</p>
+        <section className="hero">
+            <div className="hero_wrapper">
+                <div className="hero_content">
+                    <div className="hero__article">
+                        <h1 className="hero__title">Beautiful food &<br /> takeaway, <span className="color-text">delivered</span> <br />to your door.</h1>
+                        <p className="hero__paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.</p>
+                    </div>
+                    <button className="button_order">Place an Order</button>
+                    <div className="hero_rating">
+                        <div className="hero_star">
+                            <img src={getImgUrl('star.svg')} alt="" />
+                            <p className="trustpilot">Trustpilot</p>
+                        </div>
+                        <div className="hero_reviews">
+                            <p className="rating"><span className="rating-color">4.8 out of 5</span> based on 2000+ reviews</p>
+                        </div>
+                    </div>
                 </div>
+                <img src={getImgUrl('hero-image.png')} alt="hero-image" />
             </div>
         </section>
 
